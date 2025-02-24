d26900ba05359ebcea42177d53a3204649aa47e1
Waiz-bin-qasim <waizbinqasim0@gmail.com>
2025-02-24 18:39:15 +0500

    update backend and socket URLs for production environment

--- src/App.js
+++ src/App.js
@@ -204,17 +204,17 @@ function App({ domElement }) {
   // const x_api_id = 'HoWDoSfC7y1rxywh98h1J94A9k9INlRi9L8qsZ91';
 
   // Stg
-  // const backendUrl =
-  //   "https://bu4qbf7zu9.execute-api.us-east-1.amazonaws.com/dev";
-  // const x_api_id = "43KXt44PjCa7axCTLVLZb60FLrIAyA5l4YBhugmd";
-  // const socketUrl =
-  //   "wss://obz6kgfz3f.execute-api.us-east-1.amazonaws.com/production";
-
-  //   Local
-  const x_api_id = "d41d8cd98f00b204e9800998ecf8427e";
-  const backendUrl = "http://localhost:3000/dev";
+  const backendUrl =
+    "https://bu4qbf7zu9.execute-api.us-east-1.amazonaws.com/dev";
+  const x_api_id = "43KXt44PjCa7axCTLVLZb60FLrIAyA5l4YBhugmd";
   const socketUrl =
     "wss://obz6kgfz3f.execute-api.us-east-1.amazonaws.com/production";
+
+  //   Local
+  // const x_api_id = "d41d8cd98f00b204e9800998ecf8427e";
+  // const backendUrl = "http://localhost:3000/dev";
+  // const socketUrl =
+  //   "wss://obz6kgfz3f.execute-api.us-east-1.amazonaws.com/production";
 
   const [isTimerModalOpen, setIsTimerModalOpen] = useState(false);
   const [inactivityTimer, setInactivityTimer] = useState(inactiveTimer); // 3 minutes for inactivity
